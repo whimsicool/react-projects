ebfb3f23fe9bc3bfbfe949fe9676c3b42b0ec3ce
Whimsicool <whimsicool@gmail.com>
2025-07-28 21:15:49 +0700

    feat: add deployment script and update homepage in package.json; update package-lock.json with new dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/to-do-list-react/', // <- GANTI dengan nama repo kamu!
   plugins: [react()],
 })
